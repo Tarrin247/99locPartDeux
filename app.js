d4a87a0ffc23554ecfef429ef93f15f9f8192e76
Timple Basinger <tbasinger@protonmail.com>
2021-09-21 14:12:34 -0500

    Finished

--- app.js
+++ app.js
@@ -26,30 +26,29 @@ function singTheSong() {
         friendDiv.class = 'friend';
         //Attempt to Create a div for each furFriend in the array
 
-        let friendName = createElement('h3');
-        friendName.textContent[0].function() = 'furryFriends'
+        const friendName = document.createElement('h3');
+        friendName.textContent= furFriends[furryFriends];
         //Attempt to Create h3 element for each furFriend in the array
 
-        document.body.appendChild('friendDiv');
+        document.body.appendChild(friendDiv);
         //append friendDiv-div to body 
-        friendDiv.appendChild('friendName');
+        
+        friendDiv.appendChild(friendName);
         //append friendName-h3 to friendDiv
-        friendName.appendChild('para');
-        //append para-p to friendName
+        
 
         for (let j = 99; j > 0; j--) {
-
+            const para = document.createElement('p');
+            friendName.appendChild(para);
+            //append para-p to friendName
             if (j > 2) {
-                const para = document.createElement('p');
                 para.textContent = j + ' lines of code in the file, ' + j + ' lines of code ' + furFriends[furryFriends] + ' strikes one out, clears it all out,' + ' ' + + (j - 1) + ' lines of code in the file.';
             }
-            else if (j == 2) {
-                const para = document.createElement('p');
+            else if (j == 2)  {
                 para.textContent =  j + ' lines of code in the file, ' + j + ' lines of code ' + furFriends[furryFriends] + ' strikes one out, clears it all out,' + ' ' + (j - 1) + ' line of code in the file.';
             }
             else {
-                const para = document.createElement('p');  
-                para.textContent = j + ' line of code in the file, ' + j + ' line of code ' + furFriends[furryFriends] + ' strikes one out, clears it all out,' + ' ' + (j - 1) + ' lines of code in the file.';
+                  para.textContent = j + ' line of code in the file, ' + j + ' line of code ' + furFriends[furryFriends] + ' strikes one out, clears it all out,' + ' ' + (j - 1) + ' lines of code in the file.';
             }
         }
     }
